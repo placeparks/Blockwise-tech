04dd394519bef37d272e82668bf1730aea61cb3e
placeparks <86215469+placeparks@users.noreply.github.com>
2023-04-30 16:24:18 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
 import './App.css';
 import Navbar from './components/Home/Navbar';
 import Layout from './components/Home/Layout';
-import Card from './components/Home/Cards';
-import Expertise from './components/Home/Expertise';
-import Solutions from './components/Home/Solutions';
-import MetaverseBenefits from './components/Home/Metaverse-Benefits';
-import Partners from './components/Home/Partners';
-import Clients from './components/Home/Clients';
-import FAQ from './components/Home/FAQ';
-import Blog from './components/Home/Blog';
-import Contact from './components/Home/Contact';
-import Footer from './components/Home/Footer';
+import Web3 from './components/Web3.0';
+import Metaverse from './components/Metaverse';
+import Nft from './components/Nft';
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
+  console.log(window.location);
   return (
     <div>
+
+    <BrowserRouter>
     <Navbar/>
-     <Layout/>
-     <Card/>
-     <Expertise/>
-     <Solutions/>
-     <MetaverseBenefits/>
-     <Partners/>
-     <Clients/>
-     <FAQ/>
-    <Blog/>
-    <Contact/>
-    <Footer/>
+      <Routes>
+        <Route path="/" element={<Layout />}/>
+        <Route exact path="/Web3" element={<Web3 />} >
+        </Route>
+        <Route exact path="/Metaverse" element={<Metaverse />} />
+        <Route exact path="/Nft" element={<Nft />} />
+      </Routes>
+    </BrowserRouter>
+
     </div>
   );
 }
