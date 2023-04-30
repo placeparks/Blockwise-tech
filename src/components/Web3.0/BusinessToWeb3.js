2df0e629a9dfdea0fc170795fab6ba64bc2b8a24
placeparks <86215469+placeparks@users.noreply.github.com>
2023-04-30 16:38:46 -0700

    Update BusinessToWeb3.js

--- src/components/Web3.0/BusinessToWeb3.js
+++ src/components/Web3.0/BusinessToWeb3.js
@@ -8,7 +8,7 @@ export default function BusinessToWeb3(){
         <div className="row row-cols-1 row-cols-md-3 g-4">
   <div className="col">
   <div className="card" style={{height:"320px"}}>
-      <img src="/web3.0/vector.png" className={styles.image} alt="..."/>
+      <img src="/Web3.0/vector.png" className={styles.image} alt="..."/>
       <div className="card-body" style={{marginTop:"90px "}}>
         <h5 className="card-title" >Decentralization</h5>
         <p className="card-text" >One of the crucial aspects of Web 3.0 is decentralization. It is a distributed network that eliminates the involvement of any centralized authority controlling the internet. Users are responsible for their data and content.</p>
@@ -17,7 +17,7 @@ export default function BusinessToWeb3(){
   </div>
   <div className="col">
   <div className="card" style={{height:"320px"}}>
-      <img src="/web3.0/Vector1.png" className={styles.image} alt="..."/>
+      <img src="/Web3.0/Vector1.png" className={styles.image} alt="..."/>
       <div className="card-body" style={{marginTop:"90px "}}>
         <h5 className="card-title">Blockchain Technology</h5>
         <p className="card-text">Blockchain enables transparent, tamper-proof, and secure transactions. It can potentially transform the way people perform online transactions, making them more secure</p>
@@ -26,7 +26,7 @@ export default function BusinessToWeb3(){
   </div>
   <div className="col">
     <div className="card" style={{height:"320px"}}>
-      <img src="/web3.0/Vector2.png" className={styles.image} alt="..."/>
+      <img src="/Web3.0/Vector2.png" className={styles.image} alt="..."/>
       <div className="card-body" style={{marginTop:"90px"}}>
         <h5 className="card-title">Augmented Security and Privacy</h5>
         <p className="card-text">Decentralization paves the way for enhanced security as there is no single point of attack. In addition, users are empowered to choose who can access their data and how.</p>
