f9dd67bfbef24aabbc3e8fd50de93263ad4864da
placeparks <86215469+placeparks@users.noreply.github.com>
2023-05-02 13:21:40 -0700

    Update Company.js

--- src/components/Web3.0/Company.js
+++ src/components/Web3.0/Company.js
@@ -12,7 +12,7 @@ export default function Company(){
       <img src="/Web3.0/metaverse.jpg" className="card-img-top" alt="..."/>
       <div className="card-body">
         <h5 className="card-title">Web 3 Metaverse Development</h5>
-        <p className="card-text">Web 3 Metaverse Development involves building a virtual world that integrates blockchain technology and decentralized applications, where users can interact with each other and digital assets in a seamless, immersive and secure way.</p>
+        <p className="card-text" style={{textAlign:"justify"}}>Web 3 Metaverse Development involves building a virtual world that integrates blockchain technology and decentralized applications, where users can interact with each other and digital assets in a seamless, immersive and secure way.</p>
       </div>
     </div>
   </div>
@@ -21,7 +21,7 @@ export default function Company(){
       <img src="/Web3.0/wallet.png" className="card-img-top" alt="..."/>
       <div className="card-body">
         <h5 className="card-title">Web 3 Wallet Development</h5>
-        <p className="card-text">Web 3 wallet development refers to the creation of digital wallets that enable users to store, manage, and interact with various cryptocurrencies and decentralized applications (dApps) on the blockchain. These wallets prioritize user privacy and security while providing seamless user experiences.</p>
+        <p className="card-text" style={{textAlign:"justify"}}>Web 3 wallet development refers to the creation of digital wallets that enable users to store, manage, and interact with various cryptocurrencies and decentralized applications (dApps) on the blockchain. These wallets prioritize user privacy and security while providing seamless user experiences.</p>
       </div>
     </div>
   </div>
@@ -30,7 +30,7 @@ export default function Company(){
       <img src="/Web3.0/exchange.jpg" className="card-img-top" alt="..."/>
       <div className="card-body">
         <h5 className="card-title">Web 3 Exchange Development</h5>
-        <p className="card-text">Web 3 Exchange Development refers to the creation of decentralized digital marketplaces that operate on blockchain technology. These exchanges provide users with greater control, privacy, and security over their assets, and facilitate peer-to-peer transactions without intermediaries.</p>
+        <p className="card-text" style={{textAlign:"justify"}}>Web 3 Exchange Development refers to the creation of decentralized digital marketplaces that operate on blockchain technology. These exchanges provide users with greater control, privacy, and security over their assets, and facilitate peer-to-peer transactions without intermediaries.</p>
       </div>
     </div>
   </div>
@@ -39,7 +39,7 @@ export default function Company(){
       <img src="/Web3.0/gaming.jpg" className="card-img-top" alt="..."/>
       <div className="card-body">
         <h5 className="card-title">Web 3 Metaverse Game Development</h5>
-        <p className="card-text">Web 3 Metaverse Game Development refers to the creation of immersive and interconnected virtual worlds using blockchain technology and decentralized platforms. These games offer players a new level of ownership, interoperability, and economic opportunities.</p>
+        <p className="card-text" style={{textAlign:"justify"}}> Web 3 Metaverse Game Development refers to the creation of immersive and interconnected virtual worlds using blockchain technology and decentralized platforms. These games offer players a new level of ownership, interoperability, and economic opportunities.</p>
       </div>
     </div>
   </div>
@@ -48,7 +48,7 @@ export default function Company(){
       <img src="/Web3.0/contract.jpeg" style={{height:"250px"}} className="card-img-top" alt="..."/>
       <div className="card-body">
         <h5 className="card-title">Web3 Smart Contract Development</h5>
-        <p className="card-text">Web3 smart contract development involves creating self-executing digital contracts using blockchain technology. These contracts can automate the execution of agreements and eliminate the need for intermediaries, providing secure, transparent, and decentralized solutions for various industries.</p>
+        <p className="card-text" style={{textAlign:"justify"}}>Web3 smart contract development involves creating self-executing digital contracts using blockchain technology. These contracts can automate the execution of agreements and eliminate the need for intermediaries, providing secure, transparent, and decentralized solutions for various industries.</p>
       </div>
     </div>
   </div>
@@ -57,7 +57,7 @@ export default function Company(){
       <img src="/Web3.0/browser.png" style={{height:"250px", width:"410px"}}  className="card-img-top" alt="..."/>
       <div className="card-body">
         <h5 className="card-title">Web 3.0 Browsers</h5>
-        <p className="card-text">Web 3.0 browsers are the next generation of web browsers that enable decentralized web applications, also known as dApps, to interact with blockchain technology. These browsers provide enhanced privacy, security, and control over data ownership for users.</p>
+        <p className="card-text" style={{textAlign:"justify"}}>Web 3.0 browsers are the next generation of web browsers that enable decentralized web applications, also known as dApps, to interact with blockchain technology. These browsers provide enhanced privacy, security, and control over data ownership for users.</p>
       </div>
     </div>
   </div>
@@ -66,7 +66,7 @@ export default function Company(){
       <img src="/Web3.0/streaming.jpg"style={{height:"225px", width:"390px"}} className="card-img-top" alt="..."/>
       <div className="card-body">
         <h5 className="card-title">Web Streaming Services</h5>
-        <p className="card-text">Web streaming services allow users to watch TV shows, movies, and other video content online without the need to download files. Popular examples include Netflix, Hulu, and Amazon Prime Video, which offer a wide range of content for a monthly subscription fee.</p>
+        <p className="card-text" style={{textAlign:"justify"}}>Web streaming services allow users to watch TV shows, movies, and other video content online without the need to download files. Popular examples include Netflix, Hulu, and Amazon Prime Video, which offer a wide range of content for a monthly subscription fee.</p>
       </div>
     </div>
   </div>
@@ -75,7 +75,7 @@ export default function Company(){
       <img src="/Web3.0/dao.jpg" className="card-img-top" alt="..."/>
       <div className="card-body">
         <h5 className="card-title">Decentralized Autonomous Organization (DAO)</h5>
-        <p className="card-text">A Decentralized Autonomous Organization (DAO) is a type of organization that operates on a decentralized, blockchain-based platform, with rules encoded as computer programs. It operates autonomously without a central authority, allowing members to vote on decisions and allocate resources.</p>
+        <p className="card-text" style={{textAlign:"justify"}}>A Decentralized Autonomous Organization (DAO) is a type of organization that operates on a decentralized, blockchain-based platform, with rules encoded as computer programs. It operates autonomously without a central authority, allowing members to vote on decisions and allocate resources.</p>
       </div>
     </div>
   </div>
@@ -84,7 +84,7 @@ export default function Company(){
       <img src="/Web3.0/defi.jpg" style={{height:"230px"}} className="card-img-top" alt="..."/>
       <div className="card-body">
         <h5 className="card-title">Decentralized Finance (DeFi)</h5>
-        <p className="card-text">Decentralized Finance, or DeFi, refers to a financial system that operates on a decentralized, blockchain-based network, allowing for trustless peer-to-peer transactions and automated smart contract-based processes without the need for intermediaries like banks or traditional financial institutions.</p>
+        <p className="card-text" style={{textAlign:"justify"}}>Decentralized Finance, or DeFi, refers to a financial system that operates on a decentralized, blockchain-based network, allowing for trustless peer-to-peer transactions and automated smart contract-based processes without the need for intermediaries like banks or traditional financial institutions.</p>
       </div>
     </div>
   </div>
