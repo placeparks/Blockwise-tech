cafa7a194d7a35fd7b54645e00d2fc1ca0fb184b
placeparks <86215469+placeparks@users.noreply.github.com>
2023-05-07 10:49:07 -0700

    Update Layout.js

--- src/components/Home/Layout.js
+++ src/components/Home/Layout.js
@@ -4,10 +4,7 @@ import Card from './Cards';
 import Expertise from './Expertise';
 import Solutions from './Solutions';
 import MetaverseBenefits from './Metaverse-Benefits';
-import Partners from './Partners';
-import Clients from './Clients';
 import FAQ from './FAQ';
-import Blog from './Blog';
 import Contact from './Contact';
 import Footer from './Footer';
 
@@ -20,19 +17,24 @@ export default function Layout() {
 <p className={styles.topHeading}>Blockchain</p>
 <h2 className={styles.topHeading2}>Decentralizing The World</h2>
 <p className={styles.para}>Since 2016 Through Forward-Thinking Enterprise Blockchain Solutions</p>
+
     </div>
-   <div className={styles.secondSection}>
-    <h1>Embrace Web 3.0! <br/> Embrace Transformation!</h1>
-   <p>Blockchain technology is a game-changer that's disrupting industries left and right. It's time for companies and organizations to get on board or get left behind. Embrace blockchain transformation and explore its disruptive potential if you want to stay ahead of the curve. Don't be foolish enough to ignore this technology's power to revolutionize the way we do business. The benefits are too great to ignore, and those who fail to recognize that fact will find themselves struggling to keep up with the competition. It's time to be bold and blunt: either you're on board with blockchain or you're going to be left in the dust.</p>
+    <div className={styles.secondSection}>
+    <div className="card border-light mb-3" style={{maxWidth: "40rem", paddingTop:"5rem"}}>
+  <div className="card-body">
+    <h5 className="card-title" style={{fontSize:"40px"}}>Embrace Web 3.0! <br/> Embrace Transformation!</h5>
+    <p>Blockchain technology is a game-changer that's disrupting industries left and right. It's time for companies and organizations to get on board or get left behind. Embrace blockchain transformation and explore its disruptive potential if you want to stay ahead of the curve. Don't be foolish enough to ignore this technology's power to revolutionize the way we do business. The benefits are too great to ignore, and those who fail to recognize that fact will find themselves struggling to keep up with the competition. It's time to be bold and blunt: either you're on board with blockchain or you're going to be left in the dust.</p>
+  </div>
+  </div>
+
    </div>
    <Card/>
-     <Expertise/>
-     <Solutions/>
-     <MetaverseBenefits/>
+   <Expertise/>
+   <Solutions/>
+   <MetaverseBenefits/>
      <FAQ/>
     <Contact/>
     <Footer/>
-  
     </div>
   );
 }
