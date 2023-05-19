4a1cfd76f169f2c2f4622727efede77fda0de155
placeparks <86215469+placeparks@users.noreply.github.com>
2023-05-19 10:08:51 -0700

    Update FAQ.js

--- src/components/Coin/FAQ.js
+++ src/components/Coin/FAQ.js
@@ -15,8 +15,8 @@ The cost of crypto coin development services can be discussed while having direc
         <h5>(3) Which one is easier- Forking an existing cryptocurrency like Bitcoin or developing a new one from scratch?</h5>
         <p>Developing a new coin is better option than forking because it is not easy to fork Bitcoin. It would require the developers to convince the miners to accept the required code change to the blockchain. On contrary, developing a new coin is easier, even if it would require following some existing algorithm.</p>
         <h5>(4) What are the benefits you can leverage to hire a metaverse token development company?</h5>
-        <p>Smart Block Tech is a leading Metaverse token development company that assists in the creation of a part of a token used to conduct online transactions in the Metaverse. Our skilled and dedicated token developers create metaverse tokens on top of blockchain networks to aid in the execution of transactions on those networks.</p>
-        <h5>(5) Why Should you consider hiring crypto development services from Smart Block Tech?</h5>
+        <p>Blockwise Tech is a leading Metaverse token development company that assists in the creation of a part of a token used to conduct online transactions in the Metaverse. Our skilled and dedicated token developers create metaverse tokens on top of blockchain networks to aid in the execution of transactions on those networks.</p>
+        <h5>(5) Why Should you consider hiring crypto development services from Blockwise Tech?</h5>
         <p>We have been in the blockchain industry for over a decade. Our team is composed of self-driven tech experts who would create an innovative roadmap to fulfill your diverse needs. They would never let you feel disappointed.</p>
         <h5>(6) How much time does it take to develop and deploy your own cryptocurrency?</h5>
         <p>If you intend to use another currency as a base, it may only take a few weeks to create your own cryptocurrency. If you want to build one from scratch, it will take anywhere from 1 to 3 months, depending on the complexity.</p>
